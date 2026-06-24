a4d329b42703ace503781da7573f6334b53c6402
Reh Dogg Games Plus <rehdogg60@gmail.com>
2026-06-24 19:21:51 -0400

    Add Printify product catalog data

---
+++ src/data/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: "29531096",
+    title: "Baby Onesie with Colorful Bear & Back QR Code",
+    price: "$33.52",
+    url: "https://reh-dogg-arcade.printify.me/product/29531096/baby-onesie-with-colorful-bear-and-amp-back-qr-code-cute-tech-baby-bodysuit",
+    image: "https://images-api.printify.com/mockup/6a3c64b837345863c8080e11/71076/108419/baby-onesie-with-colorful-bear-back-qr-code-cute-tech-baby-bodysuit.jpg?camera_label=front&revision=1782342927168",
+  },
+  {
+    id: "29530921",
+    title: "Arcade Night Women's Skater Skirt",
+    price: "$43.99",
+    url: "https://reh-dogg-arcade.printify.me/product/29530921/arcade-night-womens-skater-skirt-retro-gaming-and-amp-dog-graphic",
+    image: "https://images-api.printify.com/mockup/6a3c638e4440fbb6bd0a2c14/43205/699/arcade-night-womens-skater-skirt-retro-gaming-dog-graphic.jpg?camera_label=front&revision=1782342612420",
+  },
+  {
+    id: "29530610",
+    title: "Retro Arcade Dog Racerback Dress",
+    price: "$42.99",
+    url: "https://reh-dogg-arcade.printify.me/product/29530610/retro-arcade-dog-racerback-dress-reh-dogg-arcade-all-over-print",
+    image: "https://images-api.printify.com/mockup/6a3c60cb2569bf1d4308d826/43004/588/retro-arcade-dog-racerback-dress-reh-dogg-arcade-all-over-print.jpg?camera_label=front&revision=1782343060359",
+  },
+  {
+    id: "29530548",
+    title: "Gold Dog Tag Necklace with Fantasy Creature Design",
+    price: "$34.95",
+    url: "https://reh-dogg-arcade.printify.me/product/29530548/gold-dog-tag-necklace-with-colorful-fantasy-creature-design",
+    image: "https://images-api.printify.com/mockup/6a3c6003bb4134320e0c41cc/253890/127684/gold-dog-tag-necklace-with-colorful-fantasy-creature-design.jpg?camera_label=front&revision=1782341657789",
+  },
+  {
+    id: "29530516",
+    title: "Retro Arcade Dog Basketball Jersey",
+    price: "$49.38",
+    url: "https://reh-dogg-arcade.printify.me/product/29530516/retro-arcade-dog-basketball-jersey-rehdogg-arcade-all-over-print",
+    image: "https://images-api.printify.com/mockup/6a3c5fa118d11c5ae408d625/79389/46754/retro-arcade-dog-basketball-jersey-rehdogg-arcade-all-over-print.jpg?camera_label=front&revision=1782341554495",
+  },
+  {
+    id: "29530423",
+    title: "Reh Dogg Arcade T-shirt",
+    price: "$30.02",
+    url: "https://reh-dogg-arcade.printify.me/product/29530423/reh-dogg-arcade-t-shirt-retro-arcade-dog-gamer-graphic-qr-code-back",
+    image: "https://images-api.printify.com/mockup/6a3c546e18d11c5ae408ce25/73204/98445/reh-dogg-arcade-t-shirt-retro-arcade-dog-gamer-graphic-qr-code-back.jpg?camera_label=front&revision=1782341304708",
+  },
+  {
+    id: "29530410",
+    title: "Arcade Pet Gamer Mug",
+    price: "$11.99",
+    url: "https://reh-dogg-arcade.printify.me/product/29530410/arcade-pet-gamer-mug-reh-dogg-arcade-retro-gaming-dog-design",
+    image: "https://images-api.printify.com/mockup/6a3c55fc92d2746b2c0f389a/65217/6405/arcade-pet-gamer-mug-reh-dogg-arcade-retro-gaming-dog-design.jpg?camera_label=front&revision=1782341244624",
+  },
+  {
+    id: "29530402",
+    title: "Arcade Nights Basketball Shorts",
+    price: "$56.37",
+    url: "https://reh-dogg-arcade.printify.me/product/29530402/arcade-nights-basketball-shorts-reh-dogg-arcade-graphic-aop",
+    image: "https://images-api.printify.com/mockup/6a3c5ce954155d223807857a/88687/51926/arcade-nights-basketball-shorts-reh-dogg-arcade-graphic-aop.jpg?camera_label=front&revision=1782341225540",
+  },
+  {
+    id: "29530398",
+    title: "High Top Sneakers - Retro Neon Hip-Hop Collage Print",
+    price: "$53.68",
+    url: "https://reh-dogg-arcade.printify.me/product/29530398/high-top-sneakers-retro-neon-hip-hop-collage-print",
+    image: "https://images-api.printify.com/mockup/6a3c5e3454155d2238078661/76220/85449/high-top-sneakers-retro-neon-hip-hop-collage-print.jpg?camera_label=both-angle&revision=1782341195599",
+  },
+  {
+    id: "29530206",
+    title: "Arcade Dog Graphic Jersey",
+    price: "$42.93",
+    url: "https://reh-dogg-arcade.printify.me/product/29530206/arcade-dog-graphic-jersey-reh-dogg-retro-gaming-tee",
+    image: "https://images-api.printify.com/mockup/6a3c5b7837345863c80807e3/101481/95988/arcade-dog-graphic-jersey-reh-dogg-retro-gaming-tee.jpg?camera_label=front&revision=1782340499611",
+  },
+];
+
+export default products;
